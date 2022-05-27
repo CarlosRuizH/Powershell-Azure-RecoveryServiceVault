e5ac01e70f513f2970cc1c121a9a92a29bec5838
CarlosRuizH <cruizhurtado@microsoft.com>
2022-05-27 10:10:12 -0400

    Minor bug corrections

--- MoveProtectedItemsToNewLRSVault-CUI.ps1
+++ MoveProtectedItemsToNewLRSVault-CUI.ps1
@@ -248,6 +248,7 @@ else {
 	}
 }
 
+
 $newVault = Get-AzRecoveryServicesVault -Name "$($newVaultName)" -ResourceGroupName $($oldRecoveryServiceVault.ResourceGroupName)
 
 # Get Backup Protection Policies from the old Vault and move them to the new Vault
